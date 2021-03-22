ef50b79a10c3237454e74b39aa9c1b5b94212eaa
Adam Carter <mail@adam-carter.com>
2021-03-22 14:15:17 -0700

    updated project.

--- src/Project7/OrCommand.hpp
+++ src/Project7/OrCommand.hpp
@@ -16,21 +16,6 @@ public:
     {
         ostringstream result{};
 
-        //AC: Oops, I didn't mean to check in the answer.  Nonetheless, you aren't allowed to 
-        //use this.  I will check this file and make sure that you write your OWN VERSION.
-        PopCommand first{ "gp2" };
-        PopCommand second{ "gp3" };
-        PushCommand store{ "gp2" };
-
-        result << first.ToAsm();
-        result << second.ToAsm();
-        result << "@" << Segment::GP2 << endl;
-        result << "D=M" << endl;
-        result << "@" << Segment::GP3 << endl;
-        result << "D=D|M" << endl;
-        result << "@" << Segment::GP2 << endl;
-        result << "M=D" << endl;
-        result << store.ToAsm();
         return result.str();
     }
 };
